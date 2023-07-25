f6b625a93678c4575eba3594cde0a01030ab8a54
abbyjo <abbyjo225@gmail.com>
2023-07-25 14:07:26 -0400

    Improved readability

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,11 @@
-//TO-DO: set up 2nd event listener for any button click 
-    //TO-DO: (if..else)when button clicked, if true next q pops up
-        //TO-DO: if false timer loses 10 seconds and next q pops up
-//TO-DO: maybe set up for loop? (for each time button pressed ^^^ happens)
-
-//TO-DO: when all q's are answered or timer = 0, game is over (game over screen)
-//TO-DO: game over screen: save initials and score (log both into localStorage)
-//TO-DO: can view high scores by clicking #highscores
-    //TO-DO: clicking highscores section reveals logged info.. ? 
-    // promble: how to log multiple high scores?? 
-//===================== CODE BELOW
+//Declarations 
 var startButton = document.querySelector("#button");
 var questionContent = document.querySelector("#content");
 var countdown = document.querySelector("#timer");
 var inputContent = document.querySelector("#input");
 var secondsLeft = 100
 
+//Function for adding new questions 
 function nextQuestion(question,a,b,c,d) {
     questionContent.textContent = `${question}`;
     inputContent.innerHTML = 
@@ -22,8 +13,9 @@ function nextQuestion(question,a,b,c,d) {
         <button id="B">${b}</button>
         <button id="C">${c}</button>
         <button id="D">${d}</button>`
-}
+};
 
+//Start of Quiz
 startButton.addEventListener("click", function() {
     var timer = setInterval(function (){
         secondsLeft--;
